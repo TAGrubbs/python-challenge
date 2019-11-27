452bfd94f43ab3c879366628d9760a8f66df0c97
TAGrubbs <toddagrubbs@gmail.com>
2019-11-26 18:31:35 -0600

    Cleaning Code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,33 +1,23 @@
 import os
 import csv
+import numpy as np
 csvpath = os.path.join('..', 'Resources', 'Pybank', 'budget_data.csv')
 
 dates= []
-pORl= []
+profitOrLoss= []
 totalPOL= []
 netvalue= 0
 months=0 
-AvgChange=0
-RAvgChange= 0
-
-with open(csvpath, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader)
-    for row in csvreader:
-        months += 1
-
-with open(csvpath, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader)
-    for row in csvreader:
-        dates.append(row[0])
-        pORl.append(int(row[1]))
-
+averageChange=0
+roundedAverageChange= 0
 isFirstRow = True
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
     for row in csvreader:
+        months += 1
+        dates.append(row[0])
+        profitOrLoss.append(int(row[1]))
         if isFirstRow:
             netvalue = int(row[1])
             isFirstRow = False
@@ -36,33 +26,25 @@ with open(csvpath, newline='') as csvfile:
             totalPOL.append(int(netvalue))
         netvalue=int(row[1])
 
-#zipdLists = zip(dates, pORl, totalPOL)
-#dictstocks = dict(zipdLists)
-
-#print(sum(totalPOL)/len(totalPOL))
-#print(months)
-import numpy as np
 
 minIndx = np.argmin(totalPOL)
-#print(dates[(minIndx+1)])
 maxIndx = np.argmax(totalPOL)
-#print(dates[(maxIndx+1)])
-AvgChange = (sum(totalPOL)/len(totalPOL))
-RAvgChange= (round(AvgChange, 2))
+averageChange = (sum(totalPOL)/len(totalPOL))
+roundedAverageChange= (round(averageChange, 2))
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(months))
-print("Total: $" + str(sum(pORl)))
-print("Average  Change: $" + str(RAvgChange))
-print("Greatest Increase in Profits: " + str(dates[maxIndx+1]) + " (" + str(pORl[(maxIndx+1)]) + ")")
-print("Greatest Decrease in Profits: " + str(dates[(minIndx+1)]) + " (" + str(pORl[(minIndx+1)]) + ")")
+print("Total: $" + str(sum(profitOrLoss)))
+print("Average  Change: $" + str(roundedAverageChange))
+print("Greatest Increase in Profits: " + str(dates[maxIndx+1]) + " (" + str(profitOrLoss[(maxIndx+1)]) + ")")
+print("Greatest Decrease in Profits: " + str(dates[(minIndx+1)]) + " (" + str(profitOrLoss[(minIndx+1)]) + ")")
 
-file = open("results.txt", "w") 
+file = open("Bank_Results.txt", "w") 
 file.write("Financial Analysis"+ '\n')
 file.write("----------------------------"+ '\n')
 file.write("Total Months: " + str(months)+ '\n')
-file.write("Total: $" + str(sum(pORl))+ '\n')
-file.write("Average  Change: $" + str(RAvgChange)+ '\n')
-file.write("Greatest Increase in Profits: " + str(dates[maxIndx+1]) + " (" + str(pORl[(maxIndx+1)]) + ")"+ '\n')
-file.write("Greatest Decrease in Profits: " + str(dates[(minIndx+1)]) + " (" + str(pORl[(minIndx+1)]) + ")"+ '\n')
+file.write("Total: $" + str(sum(profitOrLoss))+ '\n')
+file.write("Average  Change: $" + str(roundedAverageChange)+ '\n')
+file.write("Greatest Increase in Profits: " + str(dates[maxIndx+1]) + " (" + str(profitOrLoss[(maxIndx+1)]) + ")"+ '\n')
+file.write("Greatest Decrease in Profits: " + str(dates[(minIndx+1)]) + " (" + str(profitOrLoss[(minIndx+1)]) + ")"+ '\n')
 file.close() 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,57 +1,56 @@
 import os
 import csv
 
-# Identifies file with poll data
 csvpath = os.path.join('..', 'Resources', 'PyPoll', 'election_data.csv')
 poll = {}
-total_votes = 0
+totalVotes = 0
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
     for row in csvreader:
-        total_votes += 1
+        totalVotes += 1
         if row[2] in poll.keys():
             poll[row[2]] = poll[row[2]] + 1
         else:
             poll[row[2]] = 1
  
-allcandidates = []
-candidates_votes = []
+allCandidates = []
+candidatesVotes = []
 
 for key, value in poll.items():
-    allcandidates.append(key)
-    candidates_votes.append(value)
+    allCandidates.append(key)
+    candidatesVotes.append(value)
 
-percentofvote = []
-for n in candidates_votes:
-    percentofvote.append(round(n/total_votes*100, 1))
+percentOfVote = []
+for n in candidatesVotes:
+    percentOfVote.append(round(n/totalVotes*100, 1))
 
-results = list(zip(allcandidates, candidates_votes, percentofvote))
-winner_list = []
+results = list(zip(allCandidates, candidatesVotes, percentOfVote))
+winnerList = []
 
 for name in results:
-    if max(candidates_votes) == name[1]:
-        winner_list.append(name[0])
+    if max(candidatesVotes) == name[1]:
+        winnerList.append(name[0])
 
-winner = winner_list[0]
+winner = winnerList[0]
 
-if len(winner_list) > 1:
-    for w in range(1, len(winner_list)):
-        winner = winner + ", " + winner_list[w]
+if len(winnerList) > 1:
+    for w in range(1, len(winnerList)):
+        winner = winner + ", " + winnerList[w]
 
 print("Election Results" + '\n')
 print("-------------------------" + '\n')
-print("Total Votes: " + str(total_votes) + '\n')
+print("Total Votes: " + str(totalVotes) + '\n')
 print("-------------------------" + '\n')
 for entry in results:
     print(entry[0] + ": " + str(entry[2]) +"%  (" + str(entry[1]) + ')\n')
 print("-------------------------" + '\n')
 print("Winner: " + str(winner))
 
-file = open("results.txt", "w") 
+file = open("Poll_Results.txt", "w") 
 file.write("Election Results" + '\n')
 file.write("-------------------------" + '\n')
-file.write("Total Votes: " + str(total_votes) + '\n')
+file.write("Total Votes: " + str(totalVotes) + '\n')
 file.write("-------------------------" + '\n')
 for entry in results:
     file.write(entry[0] + ": " + str(entry[2]) +"%  (" + str(entry[1]) + ')\n')
